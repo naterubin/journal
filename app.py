22a38499910baaddbcb4c116a6d25f3f49306664
Nate Rubin <nrubin@appliedgeosolutions.com>
2017-11-22 10:33:56 -0500

    Proper revsion display and markdown downloading

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from glob import glob
 import os
 
-from flask import Flask, abort, request, render_template
+from flask import Flask, abort, request, render_template, send_file
 
 import markdown
 
@@ -12,9 +12,17 @@ WRITINGS_DIR = os.path.join(os.path.dirname(__file__), 'writings')
 def format_title(title):
     return title.replace("_", " ").capitalize()
 
-def get_piece(title, rev="current"):
+def current_revisions(title):
     piece_dir = os.path.join(WRITINGS_DIR, title)
+    return len(glob(os.path.join(piece_dir, "rev*.md")))
+
+def piece_html(title, rev="current"):
+    piece_dir = os.path.join(WRITINGS_DIR, title)
+
     if os.path.isdir(piece_dir):
+        rev_count = current_revisions(title)
+        revs = [str(r) for r in reversed(range(1, rev_count + 1))]
+
         with open(os.path.join(piece_dir, '{}.md'.format(rev))) as content:
             return render_template(
                 'piece.html',
@@ -22,8 +30,20 @@ def get_piece(title, rev="current"):
                 slug=title,
                 content=markdown.markdown(content.read()),
                 rev=rev,
-                revs=["2", "1"]
+                revs=revs
             )
+    abort(404)
+
+def piece_md(title, rev="current"):
+    piece_dir = os.path.join(WRITINGS_DIR, title)
+
+    if os.path.isdir(piece_dir):
+        print(title, rev)
+        return send_file(
+            os.path.join(piece_dir, rev) + ".md",
+            as_attachment=True,
+            attachment_filename="{}_{}.md".format(title, rev)
+        )
     abort(404)
 
 @app.route("/")
@@ -34,11 +54,21 @@ def home():
 
 @app.route("/<string:title>")
 def piece(title):
-    return get_piece(title)
+    return_type = request.args.get('format', 'html')
+
+    if return_type == 'md':
+        return piece_md(title)
+
+    return piece_html(title)
 
 @app.route("/<string:title>/rev/<int:rev>")
 def revision(title, rev):
-    return get_piece(title, "rev{}".format(rev))
+    return_type = request.args.get('format', 'html')
+
+    if return_type == 'md':
+        return piece_md(title, "rev{}".format(rev))
+
+    return piece_html(title, "rev{}".format(rev))
 
 @app.route("/new_piece", methods=['POST'])
 def new_piece():
@@ -49,10 +79,10 @@ def new_piece():
     piece_dir = os.path.join(WRITINGS_DIR, piece_title)
 
     if os.path.isdir(piece_dir):
-        current_revisions = len(glob(os.path.join(piece_dir, "rev*.md")))
+        rev_count = current_revisions(piece_title)
         os.rename(
             os.path.join(piece_dir, "current.md"),
-            os.path.join(piece_dir, 'rev{}.md'.format(current_revisions + 1))
+            os.path.join(piece_dir, 'rev{}.md'.format(rev_count + 1))
         )
     else:
         os.mkdir(piece_dir)
